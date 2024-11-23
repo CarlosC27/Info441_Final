555d13ebc880c2793d004230050f76ecd0743d97
rrenusree <renusree@uw.edu>
2024-11-22 16:11:42 -0800

    added simple route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import {promises as fs} from 'fs';
 
 
 import { fileURLToPath } from 'url';
@@ -18,5 +19,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get("/", async (req, res) => {
+    let fileContents = await fs.readFile("public/homepage.html")
+    res.type("html")
+    res.send(fileContents)
+})
+
+app.get('/style.css', async (req, res) => {
+    console.log("request to '/style.css', sending back css content")
+    let fileContents = await fs.readFile("public/stylesheets/style.css")
+    res.type("css")
+    res.send(fileContents)
+})
 
 export default app;
